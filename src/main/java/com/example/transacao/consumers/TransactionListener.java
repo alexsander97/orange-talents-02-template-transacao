838cbfe1f509fb319b696f841ea06859232cc0fd
alexsander97 <alexsander_camilo97@hotmail.com>
2021-04-03 02:39:45 -0300

    correções

--- src/main/java/com/example/transacao/consumers/TransactionListener.java
+++ src/main/java/com/example/transacao/consumers/TransactionListener.java
@@ -2,6 +2,8 @@ package com.example.transacao.consumers;
 
 import com.example.transacao.dtos.TransactionResponse;
 import com.example.transacao.repositories.TransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -9,13 +11,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class TransactionListener {
 
+    private static final Logger logger = LoggerFactory.getLogger(TransactionListener.class);
+
     @Autowired
     private TransactionRepository transactionRepository;
 
     @KafkaListener(topics = "${spring.kafka.topic.transactions}")
     public void listen(TransactionResponse transactionResponse) {
-        System.out.println("Chegou aqui!!");
+        logger.info("Chegou a transação..");
         transactionRepository.save(transactionResponse.toEntity());
-        System.out.println(transactionResponse);
     }
 }
